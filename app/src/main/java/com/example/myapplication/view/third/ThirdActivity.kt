03392449a739c3251cd8c6945a8e742c96ba1096
meanu <stelli.project@gmail.com>
2021-02-15 14:22:50 +0900

    Make todo add button

--- app/src/main/java/com/example/myapplication/view/third/ThirdActivity.kt
+++ app/src/main/java/com/example/myapplication/view/third/ThirdActivity.kt
@@ -1,10 +1,16 @@
 package com.example.myapplication.view.third
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AlertDialog
 import com.example.myapplication.R
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 import com.example.myapplication.databinding.ThirdLayoutBinding
+import com.example.myapplication.model.TodoModel
+import kotlinx.android.synthetic.main.dialog_layout.view.*
+import kotlinx.android.synthetic.main.third_layout.*
+import java.util.*
 
 class ThirdActivity : BindingActivity<ThirdLayoutBinding>() {
     @LayoutRes
@@ -14,7 +20,27 @@ class ThirdActivity : BindingActivity<ThirdLayoutBinding>() {
         super.onCreate(savedInstanceState)
         binding.vm = getViewModel()
         binding.setLifecycleOwner(this)
+        btn_add_todo.setOnClickListener{
+            openAddTodoDialog()
+        }
     }
 
-
+    fun openAddTodoDialog() {
+            val dialogView = layoutInflater.inflate(R.layout.dialog_layout, null)
+            val dialog = AlertDialog.Builder(this)
+                .setTitle("Add TODOLIST")
+                .setView(dialogView)
+                .setPositiveButton("CONFIRM",  {dialoginterface, i ->
+                    val title =dialogView.et_todo_title.text.toString()
+                    val description = dialogView.et_todo_desc.text.toString()
+                    val createdDate = Date().time
+                    val todoModel  = TodoModel(null, title, description, createdDate)
+                    //전달되면 ROOM에의해 삽입되면서 할당된다.
+                    val model : ThirdViewModel = getViewModel ()
+                    model.insertTodo(todoModel)
+                })
+                .setNegativeButton("CANCLE", null)
+                .create()
+            dialog.show()
+    }
 }
--- app/src/main/java/com/example/myapplication/view/third/ThirdViewModel.kt
+++ app/src/main/java/com/example/myapplication/view/third/ThirdViewModel.kt
@@ -5,8 +5,9 @@ import androidx.lifecycle.ViewModel
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.example.myapplication.TodoDAO.TodoDAO
-import com.example.myapplication.core.BaseViewModel
 import com.example.myapplication.model.TodoModel
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 
 class ThirdViewModel(private val dao: TodoDAO) : ViewModel() {
     val items: LiveData<PagedList<TodoModel>> = LivePagedListBuilder(dao.findAll(),  /* page size */ 10).build()
@@ -15,6 +16,16 @@ class ThirdViewModel(private val dao: TodoDAO) : ViewModel() {
         Thread(Runnable {
             dao.deleteUser(title)
         }).start()
+    }
 
+    fun insertTodo(todoModel: TodoModel){
+        Observable.just(todoModel)
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                dao.insertTodo(it)
+            },{
+
+            }
+            )
     }
 }
