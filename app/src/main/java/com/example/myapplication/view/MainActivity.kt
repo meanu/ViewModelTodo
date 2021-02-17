f43e06a253dfb1274ad631ef92ee5dbb261581a2
meanu <stelli.project@gmail.com>
2021-02-18 05:36:47 +0900

    Add menu onclick

--- app/src/main/java/com/example/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/view/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
@@ -24,16 +26,24 @@ class MainActivity : AppCompatActivity() {
             val nextIntent = Intent(this, secondActivity::class.java)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 startActivity(nextIntent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+
             }
         }
         button2.setOnClickListener {
             startActivity(Intent(this, ThirdActivity::class.java))
-
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_search, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            R.id.bookmark ->
+                Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
